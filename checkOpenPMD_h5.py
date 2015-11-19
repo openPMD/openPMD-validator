Fix Weighting Checks
They only apply to ED-PIC extension, this fixes the checker.

checkOpenPMD_h5.py
@@ -771,10 +771,11 @@ def check_particles(f, iteration, v, pic) :
                 time_type = f[base_path].attrs["time"].dtype.type
                 result_array += test_attr(species[record], v, "required",
                                           "timeOffset", time_type)
-                result_array += test_attr(species[record], v, "required",
-                                          "weightingPower", np.float64)
-                result_array += test_attr(species[record], v, "required",
-                                          "macroWeighted", np.uint32)
+                if pic :
+                    result_array += test_attr(species[record], v, "required",
+                                              "weightingPower", np.float64)
+                    result_array += test_attr(species[record], v, "required",
+                                              "macroWeighted", np.uint32)
                 # Attributes of the components
                 if is_scalar_record( species[record] ) : # Scalar record
                     dset = species[record]