15a7221bc85de8f80d17334d5e1031e47e418b34
Axel Huebl <axel.huebl@plasma.ninja>
2021-08-09 11:48:57 -0700

    setup.py: Support Markdown
    Properly render on PyPI

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read_requirements():
 
 
 def read_readme():
-    with open('README.md') as f:
+    with open('./README.md', encoding='utf-8') as f:
         return f.read()
 
 
@@ -23,6 +23,7 @@ setup(
     install_requires=read_requirements(),
     description='Validator and examples for openPMD format',
     long_description=read_readme(),
+    long_description_content_type='text/markdown',
     classifiers=[
         # 'Development Status :: 4 - Beta',
         'Environment :: Console',
