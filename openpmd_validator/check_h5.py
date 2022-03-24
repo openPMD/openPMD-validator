fcc6df6088bea1688c9e445ab808316ac5d50d96
Axel Huebl <axel.huebl@plasma.ninja>
2022-03-24 11:43:11 -0700

    Fix: Join openPMD paths on Windows
    Always POSIX forward slashes

--- openpmd_validator/check_h5.py
+++ openpmd_validator/check_h5.py
@@ -25,6 +25,7 @@ try:
     from collections.abc import Iterable
 except ImportError:
     from collections import Iterable
+from posixpath import join
 
 
 # version of the openPMD standard
@@ -63,6 +64,13 @@ def parse_cmd(argv):
         print("File '%s' not found!" % file_name)
         help()
     return(file_name, verbose, force_extension_pic)
+
+
+def join_path(path, other_path):
+    """ This joins openPMD internal paths.
+
+    Contrary to os.path.join, they are always forward-slashes"""
+    return join(path, other_path)
 
 
 def open_file(file_name):
@@ -570,7 +578,7 @@ def check_meshes(f, iteration, v, extensionStates):
                   "(will not search for mesh records)")
 
     if meshes_path:
-        if os.path.join( base_path, meshes_path) != ( base_path + meshes_path ):
+        if join_path( base_path, meshes_path) != ( base_path + meshes_path ):
             print("Error: `basePath`+`meshesPath` seems to be malformed "
                 "(is `basePath` absolute and ends on a `/` ?)")
             return( np.array([1, 0]) )
@@ -731,7 +739,7 @@ def check_particles(f, iteration, v, extensionStates) :
                   "(will not search for particle records)")
 
     if particles_path:
-        if os.path.join( base_path, particles_path) !=  \
+        if join_path( base_path, particles_path) !=  \
             ( base_path + particles_path ) :
             print("Error: `basePath`+`particlesPath` seems to be malformed "
                 "(is `basePath` absolute and ends on a `/` ?)")
@@ -853,7 +861,7 @@ def check_particles(f, iteration, v, extensionStates) :
                 else : # Vector record
                     # Loop over the components
                     for component_name in list(species[record].keys()):
-                        dset = species[ os.path.join(record, component_name) ]
+                        dset = species[ join_path(record, component_name) ]
                         result_array += test_component(dset, v)
 
             # weighting's attributes are fixed
